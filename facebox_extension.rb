05f57c5c4b4bfe157f8c4d2bf6b3a691d2c88d8b
Andrey Voronkov <andrey@linux-2n20.site>
2010-06-29 16:18:20 +1000

    Some improvements

--- facebox_extension.rb
+++ facebox_extension.rb
@@ -8,7 +8,7 @@ class FaceboxExtension < Spree::Extension
     # Add an extra large (xl) size to use for zooming.
     Image.attachment_definitions[:attachment][:styles] =
       { :mini => '48x48>', :small => '100x100>', :product => '240x240>',
-        :large => '600x600>', :xl => '1000x1000>' }
+        :large => '600x600>', :xl => '900x900>' }
 
     ProductsHelper.class_eval do
       def image_controls
