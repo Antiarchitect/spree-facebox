d15758f50dda205c048499795ed7a984839394d5
Andrey Voronkov <voronkovaa@gmail.com>
2010-09-04 23:32:10 -0700

    Fix from commuter. Multiple css files inclusion fixed.

--- config/initializers/facebox.rb
+++ config/initializers/facebox.rb
@@ -1,1 +1,3 @@
-Spree::Config.set(:stylesheets => Spree::Config[:stylesheets] + ',facebox,product')
+unless Spree::Config[:stylesheets].include?('facebox,product')
+  Spree::Config.set(:stylesheets => "#{Spree::Config[:stylesheets]},facebox,product")
+end
